8400c9ef90c011cf11c210e5b3171c3d4343aea0
yeongpyo <byp93@naver.com>
2018-03-07 22:21:04 +0900

    2018-03-07-22:20

---
+++ app/src/main/java/com/example/yeongpyo/studytoandroid/JAVA_Study/JAVAMainActivity.java
@@ -0,0 +1,15 @@
+package com.example.yeongpyo.studytoandroid.JAVA_Study;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yeongpyo.studytoandroid.R;
+
+public class JAVAMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_javamain);
+    }
+}
---
+++ app/src/main/java/com/example/yeongpyo/studytoandroid/KOTLIN_Study/KOTLINMainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.yeongpyo.studytoandroid.KOTLIN_Study
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.yeongpyo.studytoandroid.R
+
+class KOTLINMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlinmain)
+    }
+}
