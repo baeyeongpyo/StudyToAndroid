28158f65f0d9c4811feb611de3da174aa0c740c8
yeongpyo <byp93@naver.com>
2018-03-07 22:37:40 +0900

    2018-03-07-22:37

--- app/src/main/java/com/example/yeongpyo/studytoandroid/JAVA_Study/Study_ExpandableListView/Example_ExpandableListView.java
+++
@@ -1,8 +0,0 @@
-package com.example.yeongpyo.studytoandroid.JAVA_Study.Study_ExpandableListView;
-
-/**
- * Created by yeongpyo on 2018-03-07.
- */
-
-public class Example_ExpandableListView {
-}
---
+++ app/src/main/java/com/example/yeongpyo/studytoandroid/JAVA_Study/Study_ExpandableListView/Example_ExpandableListViewActivity.java
@@ -0,0 +1,15 @@
+package com.example.yeongpyo.studytoandroid.JAVA_Study.Study_ExpandableListView;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yeongpyo.studytoandroid.R;
+
+public class Example_ExpandableListViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example__expandable_list_view);
+    }
+}
