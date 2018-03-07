f6582d6c156cd41551f557163d34207b22933336
yeongpyo <byp93@naver.com>
2018-03-07 22:26:03 +0900

    2018-03-07-22:25

--- app/src/main/java/com/example/yeongpyo/studytoandroid/MainActivity.java
+++ app/src/main/java/com/example/yeongpyo/studytoandroid/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.yeongpyo.studytoandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.yeongpyo.studytoandroid.JAVA_Study.JAVAMainActivity;
+import com.example.yeongpyo.studytoandroid.KOTLIN_Study.KOTLINMainActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +14,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.JAVAButton).setOnClickListener(view->startActivity(new Intent(this, JAVAMainActivity.class)));
 
+        findViewById(R.id.KOTLINButton).setOnClickListener(view -> startActivity(new Intent(this, KOTLINMainActivity.class)));
 
     }
 }
