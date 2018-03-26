9b4e5e7302e0832e237f53a6d896c88e0bf1da08
yeongpyo <byp93@naver.com>
2018-03-27 01:30:08 +0900

    2018-03-27-01:30

--- app/src/main/java/com/example/yeongpyo/studytoandroid/KOTLIN_Study/text_Study/Linkfity_study.kt
+++ app/src/main/java/com/example/yeongpyo/studytoandroid/KOTLIN_Study/text_Study/Linkfity_study.kt
@@ -1,8 +1,13 @@
 package com.example.yeongpyo.studytoandroid.KOTLIN_Study.text_Study
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.Html
+import android.text.SpannableString
 import android.text.util.Linkify
 import android.widget.TextView
 import com.example.yeongpyo.studytoandroid.R
@@ -20,9 +25,17 @@ class Linkfity_study : AppCompatActivity() {
         textview.setLinkTextColor(Color.GREEN)
         textview.setText("링크 걸기, 링크 테스트")
 
+        val HtmlTag : String = "this is a test of <b>ImageGetter</b> it contains " +
+                "two images: <br/>" +
+                "<img src=\"http://developer.android.com/assets/images/dac_logo.png\"><br/>and<br/>" +
+                "<img src=\"http://developer.android.com/assets/images/icon_search.png\">"
+            var spantext : SpannableString = SpannableString(Html.fromHtml(HtmlTag,  Html.ImageGetter {var1 -> null}, null ))
+
+        textview.setText(spantext)
         var mfilter = Linkify.TransformFilter { matcher, s -> "" }
         var pattern1 = Pattern.compile("걸기")
         var pattern2 = Pattern.compile("테스트")
+
 
         Linkify.addLinks(textview, pattern1, "http://www.naver.com", null, mfilter)
         Linkify.addLinks(textview, pattern2, "http://www.google.com", null, mfilter)
